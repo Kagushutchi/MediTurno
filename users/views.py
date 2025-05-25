09abc7e02c53a6d67b54e53664487acd0f69d614
Ezequiel <ezequi45989@gmail.com>
2025-05-25 17:20:13 -0300

    Users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # o donde quieras redirigir
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
