4f464c2817ad31b9efe44d914da4067cf3de6297
Ezequiel <ezequi45989@gmail.com>
2025-06-02 00:54:55 -0300

    arreglo de stilos css

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,38 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label="Usuario")
+    password = forms.CharField(widget=forms.PasswordInput, label="Contraseña")
 
 
 class CustomUserCreationForm(UserCreationForm):
     fecha_nacimiento = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'})  # Usa el selector nativo del navegador
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        label="Fecha de Nacimiento"# Usa el selector nativo del navegador
     )
     class Meta:
         model = CustomUser
-        fields = ['username', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'email', 'password1', 'password2']  # No incluimos 'role'
-
+        fields = ['username', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Usuario',
+            'nombre': 'Nombre',
+            'apellido': 'Apellido',
+            'dni': 'DNI',
+            'fecha_nacimiento': 'Fecha de Nacimiento',
+            'email': 'Email',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar Contraseña'
+        }
+        # No incluimos 'role'
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # Sobreescribir los labels de password1 y password2
+        self.fields['password1'].label = 'Contraseña'
+        self.fields['password2'].label = 'Confirmar Contraseña'
+        
     def save(self, commit=True):
         user = super().save(commit=False)
         user.role = 'user'  # Asignamos automáticamente el rol "User"
