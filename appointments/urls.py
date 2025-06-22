a79432564d1a369f8dde928456dffc6d5b2fe9aa
Flac222 <sntsfdl02@gmail.com>
2025-06-22 20:28:43 -0300

    nuevos_models

--- appointments/urls.py
+++ appointments/urls.py
@@ -7,4 +7,5 @@ app_name = 'appointments'
 urlpatterns = [
     path('buscar/', views.buscar_medicos, name='buscar_medicos'),
     # Puedes agregar otras rutas relacionadas a turnos o citas aquí
-]
+    path('medico/<int:medico_id>/horarios/', views.obtener_horarios_medico, name='obtener_horarios_medico'),
+    ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from users.models import MedicoProfile, Especialidad, CustomUser
+from users.models import MedicoProfile, Especialidad, CustomUser, HorarioMedico
+from .models import Appointment
+from datetime import datetime, timedelta
+from collections import defaultdict
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 def buscar_medicos(request):
     query = request.GET.get('q', '')
@@ -36,3 +41,51 @@ def buscar_medicos(request):
         'especialidades': especialidades,
     }
     return render(request, 'appointments/buscar_medicos.html', context)
+
+def obtener_horarios_medico(request, medico_id):
+    medico = get_object_or_404(MedicoProfile, id=medico_id)
+    horarios = HorarioMedico.objects.filter(medico=medico)
+    turnos_tomados = Appointment.objects.filter(
+        medico=medico.user,
+        estado__in=['pendiente', 'confirmado']
+    )
+
+    horas_ocupadas = set(t.fecha_inicio for t in turnos_tomados)
+
+    from datetime import datetime, timedelta
+    from collections import defaultdict
+
+    DIAS_MAPA = {
+        'MO': 'LU',
+        'TU': 'MA',
+        'WE': 'MI',
+        'TH': 'JU',
+        'FR': 'VI',
+        'SA': 'SA',
+        'SU': 'DO',
+    }
+
+    disponibilidad = defaultdict(list)
+    hoy = datetime.now()
+
+    for i in range(7):
+        dia = hoy + timedelta(days=i)
+        dia_abrev = dia.strftime('%a').upper()[:2]
+        dia_codigo = DIAS_MAPA.get(dia_abrev)
+        if not dia_codigo:
+            continue  # Por si hay un día que no mapea
+
+        for horario in horarios:
+            if horario.dia == dia_codigo:
+                hora_actual = datetime.combine(dia.date(), horario.hora_inicio)
+                fin = datetime.combine(dia.date(), horario.hora_fin)
+                while hora_actual < fin:
+                    if hora_actual not in horas_ocupadas:
+                        disponibilidad[dia.date()].append(hora_actual.time())
+                    hora_actual += timedelta(minutes=30)
+
+    html = render_to_string('appointments/horarios_medico.html', {
+        'medico': medico,
+        'disponibilidad': dict(disponibilidad)
+    })
+    return JsonResponse({'html': html})
